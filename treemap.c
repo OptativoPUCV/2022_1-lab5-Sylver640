3c9795fbe60f39f501314d7fad124dd6be125bcd
Your Name <you@example.com>
2022-04-21 19:46:40 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -176,5 +176,11 @@ Pair * firstTreeMap(TreeMap * tree) {
 }
 
 Pair * nextTreeMap(TreeMap * tree) {
+    TreeNode* aux = tree->current;
+    if (aux->right != NULL)
+    {
+        aux = minimum(tree->current->right);
+        return aux;
+    }
     return NULL;
 }
