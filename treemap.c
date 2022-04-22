307efb6596476ef550110253fa2391b4180122b0
Your Name <you@example.com>
2022-04-21 20:19:16 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -179,9 +179,10 @@ Pair * upperBound(TreeMap * tree, void* key) {
         {
             ub_node = ub_node->right;
             if (tree->lower_than(upper_ubNode->pair->key, ub_node->pair->key) == 1)
-            {
                 upper_ubNode = ub_node;
-            }
+            else
+                if (tree->lower_than(upper_ubNode->pair->key, ub_node->pair->key) > 1 || tree->lower_than(upper_ubNode->pair->key, ub_node->pair->key) < 1)
+                    upper_ubNode = ub_node;
         }
         else
         {
