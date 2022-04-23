454019dd781ab5c1f6006f948796a6e44ff14396
Your Name <you@example.com>
2022-04-23 19:21:14 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -189,7 +189,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
                 printf("key: %d\n", *(int*) auxUbNode->pair->key);
                 continue;
             }
-            if (tree->lower_than(ubNode->pair->key, auxUbNode->pair->key) == 1)
+            if (tree->lower_than(key, auxUbNode->pair->key) == 1)
             {
                 printf("key dentro de if = 0: %d\n", *(int*) auxUbNode->pair->key);
                 auxUbNode = ubNode;
