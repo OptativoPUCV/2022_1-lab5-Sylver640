6556472b36d93b8f13ed2fcbea82195d9360bf41
Your Name <you@example.com>
2022-04-23 19:12:44 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -191,7 +191,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
             if (tree->lower_than(ubNode->pair->key, auxUbNode->pair->key) == 1 && tree->lower_than(ubNode->pair->key, key) == 1)
             {
                 auxUbNode = ubNode;
-                printf("key: %d\n", *(int*) auxUbNode->pair-key);
+                printf("key: %d\n", *(void*) auxUbNode->pair->key);
             }
             else continue;
         }
